6b80774014048902dd3b2c8810c278c7231c9294
javi45874 <javiignacia52@gmail.com>
2024-04-11 20:10:44 -0400

    estaba probando codigos

--- pruebami.py
+++ pruebami.py
@@ -1,1 +1,6 @@
-otracos
+#asi se ingresan los comentarios en python
+#la funcion print muentra lo que escribi en la consola
+# sentencia para solicitar el nombre
+nombre=input("por favor, ingresa tu nombre")
+#input permite ingresar una variable y almacenarla
+print("bienvenido,"+nombre+"!")
