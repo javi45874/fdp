b24aa8aafb66fa02f1f5009fc3815a833edd4dd8
javii45874 <javiignacia52@gmail.com>
2024-05-16 20:13:55 -0400

    clase 16 de mayo

---
+++ lista.py
@@ -0,0 +1,36 @@
+# las cosas que estan dentro de la lista se comienzan a enumerar desde el 0
+#create
+#read
+#update
+#delete
+#append me agrega el elemento que esta entre parentesis al final de la lista
+import random # se importo de una libreria externa la funcion de random
+
+lista=[41,62,24,25,87]
+
+# for elemento in lista:
+#     print (elemento)
+
+# print(lista[0]+lista[4])
+# lista.append(22) 
+# for elemento in lista:
+#     print (elemento)
+# num= random.randint(0,50)
+# num2= random.randint(0,50)
+# lista.append(lista[0]+lista[2])
+# lista.append(num)
+# lista.append(num2)
+# for elemento in lista:
+#     print (elemento)
+
+#agregar un numero a la lista, evitando que se repita
+# bingo=[24,36,24,28,36]
+# comodin=random.randint(10,99)
+
+# while comodin in bingo:
+#     print("comodin repetido")
+#     comodin=random.randint(10,99)
+# bingo.append(comodin)
+
+# for num in bingo:
+#     print(num)
